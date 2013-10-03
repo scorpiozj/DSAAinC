1dd4d65e908eaa4dac099a7205e414c05b89aa01
Charles Zhu <scorpiozj@gmail.com>
2013-10-03 21:41:23 +0800

    initial MaxSubSum Project

---
+++ Ch2/MaxSubSum/MaxSum/MaxSum/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MaxSum
+//
+//  Created by scorpiozj on 10/3/13.
+//  Copyright (c) 2013 scorpiozj. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
