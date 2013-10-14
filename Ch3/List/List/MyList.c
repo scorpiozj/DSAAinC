4299e7fa8a7d096958bba577d1813519f41a0621
Charles Zhu <scorpiozj@gmail.com>
2013-10-15 07:07:40 +0800

    Ch2 List

--- Ch3/List/List/MyList.c
+++ Ch3/List/List/MyList.c
@@ -8,3 +8,70 @@
 
 #include <stdio.h>
 #include "MyList.h"
+#include <stdlib.h>
+
+struct Node {
+    ElementType Element;
+    Position Next;
+};
+
+List MakeEmpty(List L);
+
+int IsEmpty(List L)
+{
+    return (L->Next == NULL);
+}
+
+int IsLast(Position P, List L)
+{
+    return (P->Next == NULL);
+}
+Position Find(ElementType X, List L)
+{
+    Position pos = L->Next;
+    while (pos != NULL)
+    {
+        if (pos->Element == X)
+        {
+            break;
+        }
+        pos = pos->Next;
+    }
+    
+    return pos;
+}
+void Insert(ElementType X, List L, Position P)
+{
+    Position TempCell = malloc(sizeof(Position));
+    if (TempCell != NULL)
+    {
+        TempCell->Element = X;
+        TempCell->Next = P->Next;
+        P->Next = TempCell;
+    }
+}
+void DeleteList(List L);
+Position FindPrevious(ElementType X, List L)
+{
+    Position p = L->Next;
+    while (p != NULL && p->Element != X)
+    {
+        p = p->Next;
+    }
+    return p;
+}
+void Delete(ElementType X, List L)
+{
+    Position previous = FindPrevious(X, L);
+    
+    if (previous != NULL)
+    {
+        Position current = previous->Next;
+        previous->Next = current->Next;
+    }
+}
+
+Position Header(List L);
+Position First(List L);
+Position Advance(Position P);
+ElementType Retrive(Position P);
--- Ch3/List/List/MyList.h
+++ Ch3/List/List/MyList.h
@@ -8,7 +8,24 @@
 
 #ifndef List_MyList_h
 #define List_MyList_h
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+
+typedef int ElementType;
 
 
+List MakeEmpty(List L);
+int IsEmpty(List L);
+int IsLast(Position P, List L);
+Position Find(ElementType X, List L);
+void Insert(ElementType X, List L, Position P);
+void DeleteList(List L);
+void Delete(ElementType X, List L);
+Position Header(List L);
+Position First(List L);
+Position Advance(Position P);
+ElementType Retrive(Position P);
 
 #endif
